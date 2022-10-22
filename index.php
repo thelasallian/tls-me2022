45f8c30c929042c40edf717671ecd06b3928aea9
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-10-22 21:33:27 +0800

    Undo last commit. Heroku deploy is working.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <?php require_once 'modules/head.php' ?>
 <body>
-    <h1>Testing Heroku</h1>
+    
 
     <!-- Bootstrap JS Bundle and Custom JS Script File -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
