59bb96866eaf6f5251b1229fea7b2010c689e369
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-10-22 20:54:19 +0800

    Added favicons

--- modules/head.php
+++ modules/head.php
@@ -2,6 +2,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Make-up Elections 2022 | The LaSallian</title><title>Bootstrap demo</title>
+
+    <!-- Favicons -->
+    <link rel="icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" sizes="32x32" />
+    <link rel="icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" />
     
     <!-- Bootstrap and Custom Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
