98b0cc9609211bb523bca0cc3e2f035e063608e8
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-10-22 20:47:12 +0800

    Moved entire <head> in separate PHP file

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Make-up Elections 2022 | The LaSallian</title>
-</head>
+<?php require_once 'modules/head.php' ?>
 <body>
 
 </body>
