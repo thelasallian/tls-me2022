e11263674beae5c0a708ff0d0edc84853e6c38e3
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-10-22 20:49:24 +0800

    Added other JS scripts at the end of <body>

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 <html lang="en">
 <?php require_once 'modules/head.php' ?>
 <body>
+    
 
+    <!-- Bootstrap JS Bundle and Custom JS Script File -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
