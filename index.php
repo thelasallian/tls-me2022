b9a89211b87c5247d553ff12fd99244da326377f
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-11-02 16:45:44 +0800

    Added responsive container, columns, and sections

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once 'modules/head.php' ?>
+<?php require_once 'modules/head.php'?>
 <body>
-    
+    <main class="container">
+        <!-- 
+            - row-cols-md-2:  On the md breakpoint, there are 2 columns, except Vox Populi.
+            - row-cols-1:     When it gets smaller, it becomes 1 column.
+            - g-4:            Adds the gutter (gap) between columns.
+         -->
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <!-- 
+                col-md-12:          On the md breakpoint, only Vox Populi spans two columns 
+                order-5 order-md-1: On the md breakpoint, Vox Populi appears on top. But in mobile, it's last
+            -->
+            <div class="col-md-12 order-5 order-md-1"><section>Vox Populi</section></div>
+            <div class="col order-1"><section>Head-to-Head</section></div>
+            <div class="col order-2"><section>Fast Talk: Yes or No</section></div>
+            <div class="col order-3"><section>Fast Talk: 0.0 to 4.0</section></div>
+            <div class="col order-4"><section>Features</section></div>
+        </div>
+    </main>
 
     <!-- Bootstrap JS Bundle and Custom JS Script File -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
