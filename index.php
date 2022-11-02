25a400041f227d62345598eaf5384438ebfd52c4
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-11-02 17:05:05 +0800

    Added comments to easily identify sections and column groups

--- index.php
+++ index.php
@@ -15,17 +15,37 @@
                 order-5 order-md-1: On the md breakpoint, Vox Populi appears on top. But in mobile, it's last
             -->
             <div class="col-md-12 order-5 order-md-1">
-                <section id="vox-populi">Vox Populi</section>
+                <!-- Vox Populi -->
+                <section id="vox-populi">
+                    Vox Populi
+
+                </section>
             </div>
             <!-- Column wrapping HTH and FT: 0.0 to 4.0 -->
             <div class="col order-1">
-                <section id="head-to-head" class="mb-4">Head-to-Head</section>
-                <section id="ft-rate">Fast Talk: 0.0 to 4.0</section>
+                <!-- Head to Head -->
+                <section id="head-to-head" class="mb-4">
+                    Head-to-Head
+
+                </section>
+                <!-- Fast Talk: 0.0 to 4.0 -->
+                <section id="ft-rate">
+                    Fast Talk: 0.0 to 4.0
+
+                </section>
             </div>
             <!-- Column wrapping FT: Yes or No and Features -->
             <div class="col order-2">
-                <section id="ft-yes-or-no" class="mb-4">Fast Talk: Yes or No</section>
-                <section id="features">Features</section>
+                <!-- Fast Talk: Yes or No -->
+                <section id="ft-yes-or-no" class="mb-4">
+                    Fast Talk: Yes or No
+                
+                </section>
+                <!-- Features -->
+                <section id="features">
+                    Features
+
+                </section>
             </div>
         </div>
     </main>
