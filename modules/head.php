b96d8d3c81cf3f6976a873f79c0d634b3c77f5ca
Ronn Parcia <93105085+ronnparcia@users.noreply.github.com>
2022-11-02 17:34:59 +0800

    Path fix for Heroku: "css/styles.css" to "/css/styles.css"

--- modules/head.php
+++ modules/head.php
@@ -10,7 +10,7 @@
     
     <!-- Bootstrap and Custom Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="/css/styles.css">
 
     <!-- JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
