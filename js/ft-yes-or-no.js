5b4785928a6be4a8c04e071329159f6fb5883f4a
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-11-07 23:54:13 +0800

    Added code for triggering color change on dropdown change

--- js/ft-yes-or-no.js
+++ js/ft-yes-or-no.js
@@ -8,3 +8,33 @@ $.ajax({
         candidates = data.candidates;
     }
 });
+
+// When #issues-dropdown changes, trigger changes
+$("#issues-dropdown").on('change', function() {
+    // Get currently selection option in dropdown
+    var selected_option = $("#issues-dropdown").val();
+    
+    // Trigger animation
+    $("li").addClass("animate__animated animate__flip");
+    setTimeout(function(){
+        $('li').removeClass('animate__animated animate__flip');
+    },1000);
+    $("#current-selection").text(selected_option);
+
+    // Iterate through yes/no data for each candidate based on surname
+    $.each(yes_no_data, function( key, value ){
+        // Replace any spaces with dashes, if any
+        var surname_id = value["surname"];
+        surname_id = surname_id.replaceAll(' ', '-');
+
+        // Reset red/green colors
+        $("#" + surname_id).removeClass("text-success text-danger");
+        
+        // Re-add red/green color based on Yes or No answer
+        if (value[selected_option] == "Y") {
+            $("#" + surname_id).addClass("text-success");
+        } else {
+            $("#" + surname_id).addClass("text-danger");
+        }
+    })
+});
