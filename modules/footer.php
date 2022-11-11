853355ba1784fff0dc08c8e9046f74297c8b68a7
elliamaeang <elliamae_ang@dlsu.edu.ph>
2022-11-11 19:22:04 +0800

    Added header design credit + Bold text for staffer sections

--- modules/footer.php
+++ modules/footer.php
@@ -7,9 +7,9 @@
         <p class="about"><b>The LaSallian</b> is the official student publication of De La Salle University. It is of the students, by the students, and for the students. Our student writers, photographers, videographers, artists, and web managers are committed to the 62-year tradition of journalistic excellence and issue-oriented critical thinking.</p>
         <p class="copyright">Copyright © The LaSallian 2022</p>
         <p class="credit">
-            Content by the University, Photo, Layout, and Arts &amp; Graphics sections<br>
-            Website developed by Ronn Parcia, Angelo Guerra, &amp; Elliamae Ang from the Web section<br>
-            Header design by _________ from the Arts &amp; Graphics section<br>
+            Content by the <b>University, Photo, Layout,</b> and <b>Arts &amp; Graphics</b> sections<br>
+            Website developed by Ronn Parcia, Angelo Guerra, &amp; Elliamae Ang from the <b>Web</b> section<br>
+            Header design by Addie Holgado from the <b>Arts &amp; Graphics</b> section<br>
             Icons by Freepik from Flaticon
         </p>
     </div>
