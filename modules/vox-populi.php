ee06f7621b225dd6b163257aa615734ad28fad7c
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-11-11 21:56:44 +0800

    Added code to generate images and overlays

--- modules/vox-populi.php
+++ modules/vox-populi.php
@@ -10,6 +10,15 @@
     $vp_photos = json_decode(file_get_contents('./json/vox-populi.json'), true);
     ?>
     <div class="vp-wrapper">
-        
+        <?php foreach ($vp_photos as $photo): ?>
+            <div class="vp-photo-wrapper">
+                <div class="vp-photo-overlay">
+                    <p class="vp-quote"><?php echo $photo["answer"]; ?></p>
+                    <span class="vp-name"><?php echo $photo["name"]; ?></span><br/>
+                    <span class="vp-name"><?php echo $photo["id"]; ?></span>
+                </div>
+                <img src="<?php echo $photo["filepath"]; ?>?" alt="" class="vp-photo">
+            </div>
+        <?php endforeach; ?>
     </div>
 </div>
