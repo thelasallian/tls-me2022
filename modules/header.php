669f943a00271739314082de63726a27b84db957
elliamaeang <elliamae_ang@dlsu.edu.ph>
2022-11-11 20:16:04 +0800

    Create header.php

---
+++ modules/header.php
@@ -0,0 +1,11 @@
+<div class="header-container">
+    <div id="bg">
+        <div id="content">
+            <img id="logo" src="assets/tls-star.svg"/>
+            <h1>In transition</h1>
+            <h2>Make-Up Elections 2022</h2>
+            <p><b>The LaSallian</b>'s Special Coverage</p>
+        </div>
+        <img id="banner" src="assets/mue-header-banner.png"/>
+    </div>
+</div>
