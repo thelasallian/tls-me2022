7c913f9a88c1cc2f3edc193cbce8f18f1452a911
elliamaeang <elliamae_ang@dlsu.edu.ph>
2022-11-11 23:05:07 +0800

    Placed text inside #text div

--- modules/header.php
+++ modules/header.php
@@ -3,9 +3,11 @@
         <img id="banner" src="assets/mue-header-banner.png"/>
         <div id="content">
             <img id="logo" src="assets/tls-star.svg"/>
-            <h1>In transition</h1>
-            <h2>Make-Up Elections 2022</h2>
-            <p><b>The LaSallian</b>'s Special Coverage</p>
+            <div id="text">
+                <h1>In transition</h1>
+                <h2>Make-Up Elections 2022</h2>
+                <p><b>The LaSallian</b>'s Special Coverage</p>
+            </div>
         </div>
     </div>
 </div>
