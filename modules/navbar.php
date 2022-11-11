3753112772d2d1ee3183a0f0cbb7c00ea8bb643d
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-11-11 23:03:29 +0800

    Added navbar code

---
+++ modules/navbar.php
@@ -0,0 +1,28 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="">
+            <img src="assets/tls-wordmark-white.svg" alt="TLS Name Logo">
+        </a>
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#toggleMobileMenu"
+            aria-controls="toggleMobileMenu"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="toggleMobileMenu">
+            <ul class="navbar-nav ms-auto text-center">
+                <li><a class="nav-link" href="#vox-populi">VOX POPULI</a></li>
+                <li><a class="nav-link" href="#head-to-head">HEAD-TO-HEAD</a></li>
+                <li><a class="nav-link" href="#ft-yes-or-no">YES OR NO</a></li>
+                <li><a class="nav-link" href="#ft-rate">0.0 TO 4.0</a></li>
+                <li><a class="nav-link" href="#features">FEATURES</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
