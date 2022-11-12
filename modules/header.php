b9f3e5ef472b735d10a843daedf5706449c0e881
elliamaeang <elliamae_ang@dlsu.edu.ph>
2022-11-12 20:45:47 +0800

    Rearranged items, renamed classes/ids, and added new div

--- modules/header.php
+++ modules/header.php
@@ -1,5 +1,6 @@
-<div class="header-container">
-    <div id="bg">
+<div id="header">
+    <div id="bg"></div>
+    <div id="container">
         <img id="banner" src="assets/mue-header-banner.png"/>
         <div id="content">
             <img id="logo" src="assets/tls-star.svg"/>
