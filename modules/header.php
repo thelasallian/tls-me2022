d2ae1bd422a7ad87fe34fcbb778027472ea9dc38
elliamaeang <elliamae_ang@dlsu.edu.ph>
2022-11-12 21:02:34 +0800

    Capitalized T in "In Transition"

--- modules/header.php
+++ modules/header.php
@@ -5,7 +5,7 @@
         <div id="content">
             <img id="logo" src="assets/tls-star.svg"/>
             <div id="text">
-                <h1>In transition</h1>
+                <h1>In Transition</h1>
                 <h2>Make-Up Elections 2022</h2>
                 <p><b>The LaSallian</b>'s Special Coverage</p>
             </div>
