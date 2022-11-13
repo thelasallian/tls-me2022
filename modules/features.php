388ddd91be77badefec3f5aca60e04184c71cfc6
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-11-13 13:38:32 +0800

    Fixed Article Hyperlink

--- modules/features.php
+++ modules/features.php
@@ -5,9 +5,9 @@
 
 <div class="section-body">
     <div class="row row-cols-1 row-cols-md-1 g-2">
-        <!-- PHP Loop to Render Feature Cards -->
+        <!-- PHP Loop to Render Feature Article Cards -->
         <?php
-            $features = json_decode(file_get_contents('./json/features.json'), true);
+            $features = json_decode(file_get_contents('./json/feature-articles.json'), true);
 
             foreach($features as $item) { // foreach element in $arr
                 $title = $item['title'];
@@ -16,19 +16,19 @@
                 $link = $item['link'];
         ?>
                     <div class="col">
-                        <a href="<?php echo $link; ?>" target="_blank">
-                            <div class="card border-0">
-                                <div class="row g-0">
-                                    <div class="col-5 col-sm-4 col-lg-5 p-2">
-                                        <img src="<?php echo $visual; ?>" alt="" class="card-img">
-                                    </div>
-                                    <div class="card-body d-flex flex-column justify-content-center">
+                        <div class="card border-0">
+                            <div class="row g-0">
+                                <div class="col-5 col-sm-4 col-lg-5 p-2">
+                                    <img src="<?php echo $visual; ?>" alt="" class="card-img">
+                                </div>
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <a href="<?php echo $link; ?>" target="_blank">
                                         <h5 class="card-title"><?php echo $title; ?></h5>
-                                        <p class="card-text"><?php echo $byline; ?></p>
-                                    </div>
+                                    </a>
+                                    <p class="card-text"><?php echo $byline; ?></p>
                                 </div>
                             </div>
-                        </a>
+                        </div>
                     </div>
                 <?php } ?>
             </div>
