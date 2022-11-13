2cfff3f32f0500770e67650f95933c6d773f2834
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-11-13 09:00:48 +0800

    Updated credit

--- modules/footer.php
+++ modules/footer.php
@@ -7,10 +7,11 @@
         <p class="about"><b>The LaSallian</b> is the official student publication of De La Salle University. It is of the students, by the students, and for the students. Our student writers, photographers, videographers, artists, and web managers are committed to the 62-year tradition of journalistic excellence and issue-oriented critical thinking.</p>
         <p class="copyright">Copyright © The LaSallian 2022</p>
         <p class="credit">
-            Content by the <b>University, Photo, Layout,</b> and <b>Arts &amp; Graphics</b> sections<br>
-            Website developed by Ronn Parcia, Angelo Guerra, &amp; Elliamae Ang from the <b>Web</b> section<br>
-            Header design by Addie Holgado from the <b>Arts &amp; Graphics</b> section<br>
-            Icons by Freepik from Flaticon
+        Header Art by Addie Holgado<br/>
+        Vox Populi by Nelcze Zulueta, Austin Cruz, and Angekyla Barroquillo<br/>
+        Infographic by Maxine Lee<br/>
+        Website by Ronn Parcia, Angelo Guerra, and Elliamae Ang<br/><br/>
+        Icons by Freepik from Flaticon
         </p>
     </div>
 </div>
