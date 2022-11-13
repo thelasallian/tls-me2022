cfb142ada988c531af8e3d3a8fa869798069a207
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-11-13 11:50:47 +0800

    Added pills and tabs

--- modules/head-to-head.php
+++ modules/head-to-head.php
@@ -4,20 +4,43 @@
 </div>
 
 <div class="section-body">
-    <?php
-        // Convert JSON to associative array (true parameter)
-        $hth_data = json_decode(file_get_contents('./json/hth-articles.json'), true);
+    <!-- Pills -->
+    <ul class="nav nav-pills" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="executive-board-tab" data-bs-toggle="tab" data-bs-target="#executive-board-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Executive Board</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="college-presidents-tab" data-bs-toggle="tab" data-bs-target="#college-presidents-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">College Presidents</button>
+        </li>
+    </ul>
 
-        // TEMPORARYL To display each array element
-        echo '<ol>';
-        foreach ($hth_data as $i) {
-            
-            echo '<li>';
-            echo $i["position"].'<br/>';
-            echo $i["title"].'<br/>';
-            echo $i["byline"].'<br/>';
-            echo '</li>';
-        }
-        echo '</ol>';
-    ?>
+    <!-- Tab Content -->
+    <div class="tab-content" id="myTabContent">
+        <!-- Executive Board -->
+        <div class="tab-pane fade show active" id="executive-board-pane" role="tabpanel" aria-labelledby="executive-board-tab" tabindex="0">
+            EB
+        </div>
+        <!-- College Presidents -->
+        <div class="tab-pane fade" id="college-presidents-pane" role="tabpanel" aria-labelledby="college-presidents-tab" tabindex="0">
+            CP
+        </div>
+    </div>
+
+
+    <?php
+// Convert JSON to associative array (true parameter)
+$hth_data = json_decode(file_get_contents('./json/hth-articles.json'), true);
+
+// TEMPORARYL To display each array element
+echo '<ol>';
+foreach ($hth_data as $i) {
+
+    echo '<li>';
+    echo $i["position"] . '<br/>';
+    echo $i["title"] . '<br/>';
+    echo $i["byline"] . '<br/>';
+    echo '</li>';
+}
+echo '</ol>';
+?>
 </div>
