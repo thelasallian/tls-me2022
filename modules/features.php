50077441ae7b8bf07c852e9e9774f731704a08cd
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-11-13 15:53:20 +0800

    Added Features Code and PHP Looping

--- modules/features.php
+++ modules/features.php
@@ -4,33 +4,28 @@
 </div>
 
 <div class="section-body">
-    <div class="row row-cols-1 row-cols-md-1 g-2">
+    <div class="row">
         <!-- PHP Loop to Render Feature Article Cards -->
-        <?php
-            $features = json_decode(file_get_contents('./json/feature-articles.json'), true);
+        <?php $features = json_decode(file_get_contents('./json/feature-articles.json'), true);
 
             foreach($features as $item) { // foreach element in $arr
                 $title = $item['title'];
                 $byline = $item['byline'];
                 $visual = $item['visual'];
                 $link = $item['link'];
-        ?>
-                    <div class="col">
-                        <div class="card border-0">
-                            <div class="row g-0">
-                                <div class="col-5 col-sm-4 col-lg-5 p-2">
-                                    <img src="<?php echo $visual; ?>" alt="" class="card-img">
-                                </div>
-                                <div class="card-body d-flex flex-column justify-content-center">
-                                    <a href="<?php echo $link; ?>" target="_blank">
-                                        <h5 class="card-title"><?php echo $title; ?></h5>
-                                    </a>
-                                    <p class="card-text"><?php echo $byline; ?></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
+            ?>
+            
+            <div class="feature-article-card">
+                <div class="feature-visual-wrapper">
+                    <img src="<?php echo $visual; ?>" alt="" class="feature-visual">
+                </div>
+                <div class="feature-article-details">
+                    <a href="<?php echo $link; ?>" target="_blank">
+                        <h2><?php echo $title; ?></h2>
+                    </a>
+                    <p><?php echo $byline; ?></p>
+                </div>
             </div>
+            <?php } ?>
     </div>
 </div>
