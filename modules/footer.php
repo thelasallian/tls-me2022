178ed7574c86e2ac7eaec4ca8a31c74e3a6cd08e
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-11-13 09:05:22 +0800

    Added website dev's github links

--- modules/footer.php
+++ modules/footer.php
@@ -10,7 +10,9 @@
         Header Art by Addie Holgado<br/>
         Vox Populi by Nelcze Zulueta, Austin Cruz, and Angekyla Barroquillo<br/>
         Infographic by Maxine Lee<br/>
-        Website by Ronn Parcia, Angelo Guerra, and Elliamae Ang<br/><br/>
+        Website by <a href="https://github.com/ronnparcia" target="_blank">Ronn Parcia</a>, 
+        <a href="https://github.com/angelocguerra" target="_blank">Angelo Guerra</a>, and 
+        <a href="https://github.com/elliamaeang" target="_blank">Elliamae Ang</a><br/><br/>
         Icons by Freepik from Flaticon
         </p>
     </div>
